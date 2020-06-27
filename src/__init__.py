276d3093e329b8beaa841c44e53d6a3c1ff8aedf
haris0 <hari.surrisyad@gmail.com>
2020-06-27 08:02:33 +0700

    add secret key

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_cors import CORS
+import os
 
 UPLOAD_FOLDER = './src/static/upload/'
 
@@ -12,3 +13,5 @@ bootstrap = Bootstrap(src)
 src.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 cors = CORS(src, resources={r"/api/*": {"origins": "*"}})
+
+src.config['SECRET_KEY'] =  os.urandom(12).hex()
